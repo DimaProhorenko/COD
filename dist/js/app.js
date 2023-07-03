11b4e48947df9c93caf288cca1bbcdaff334f949
DimaProhorenko <dima.study.web@gmail.com>
2023-07-03 17:04:42 +0300

    ADD mobile menu

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,18 @@
 const burger = document.querySelector('.burger');
+const headerMenu = document.querySelector('.header__nav');
 
 const burgerHandler = () => {
     burger.classList.toggle('open');
+    headerMenuHandler();
+}
+
+const headerMenuHandler = () => {
+    const height = headerMenu.offsetHeight;
+    if (height <= 1) {
+        headerMenu.style.height = headerMenu.scrollHeight + 'px';
+    } else {
+        headerMenu.style.height = 0;
+    }
 }
 
 burger.addEventListener('click', burgerHandler);
